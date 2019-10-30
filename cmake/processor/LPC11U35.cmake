9c5cafcba2ab78c00efc9cf10f03fbf257a9784b
messi1 <juergen.messerer@sunrise.ch>
2019-10-30 22:54:57 +0100

    Add a generic cortexM0 linker script

--- cmake/processor/LPC11U35.cmake
+++ cmake/processor/LPC11U35.cmake
@@ -7,9 +7,9 @@
 target_compile_options(${PROJECT_NAME}
         PRIVATE
                 -mthumb
-                -mcpu=cortex-m0plus
-                -mtune=cortex-m0plus   # https://gcc.gnu.org/onlinedocs/gcc-4.9.4/gcc/ARM-Options.html
-                -march=armv6-m         # Check https://de.wikipedia.org/wiki/ARM_Cortex-M#Cortex-M0+
+                -mcpu=cortex-m0
+                -mtune=cortex-m0   # https://gcc.gnu.org/onlinedocs/gcc-4.9.4/gcc/ARM-Options.html
+                -march=armv6-m     # Check https://de.wikipedia.org/wiki/ARM_Cortex-M#Cortex-M0
 )
 
 #target_compile_definitions(${PROJECT_NAME}
@@ -24,8 +24,7 @@ target_include_directories(${PROJECT_NAME}
 
 target_link_options(${PROJECT_NAME}
         PRIVATE
-                $<$<CXX_COMPILER_ID:Clang>:-T ${CMAKE_SOURCE_DIR}/lib/linker/lpc11u35_clang.ld>
-                $<$<CXX_COMPILER_ID:GNU>:-T ${CMAKE_SOURCE_DIR}/lib/linker/lpc11u35_gcc.ld>
+            -T ${CMAKE_SOURCE_DIR}/lib/linker/cortexM0_plus.ld
 )
 
 add_custom_target(Linkerscripts SOURCES
