8e5ecfec41373451e35621d3ab7027f9b3308096
bbv_JuergenMesserer <juergen.messerer@bbv.ch>
2020-03-05 22:08:50 +0100

    Removed srec custom target. Fixed the sym target

--- cmake/generate-binary-file.cmake
+++ cmake/generate-binary-file.cmake
@@ -1,39 +1,37 @@
 # cmake script for generating binary and hex format and for flashing
 
-add_custom_command(OUTPUT ${PROJECT_NAME}.srec
-    DEPENDS ${PROJECT_NAME}
-    COMMAND ${CMAKE_OBJCOPY} -Osrec ${PROJECT_NAME} ${PROJECT_NAME}.srec
-)
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(NM_OPT -C -n -S)
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(NM_OPT -C -l -n -S)
+endif()
+
 add_custom_command(OUTPUT ${PROJECT_NAME}.hex
     DEPENDS ${PROJECT_NAME}
-    COMMAND ${CMAKE_OBJCOPY} -Oihex ${PROJECT_NAME} ${PROJECT_NAME}.hex
+    COMMAND ${CMAKE_OBJCOPY} -O ihex ${PROJECT_NAME} ${PROJECT_NAME}.hex
 )
 add_custom_command(OUTPUT ${PROJECT_NAME}.bin
     DEPENDS ${PROJECT_NAME}
-    COMMAND ${CMAKE_OBJCOPY} -Obinary ${PROJECT_NAME} ${PROJECT_NAME}.bin
+    COMMAND ${CMAKE_OBJCOPY} -O binary ${PROJECT_NAME} ${PROJECT_NAME}.bin
 )
-add_custom_command(OUTPUT ${PROJECT_NAME}.lst
+add_custom_command(OUTPUT ${PROJECT_NAME}.S
     DEPENDS ${PROJECT_NAME}
-    COMMAND ${CMAKE_OBJDUMP} -S ${PROJECT_NAME} > ${PROJECT_NAME}.lst
+    COMMAND ${CMAKE_OBJDUMP} -dS ${PROJECT_NAME} > ${PROJECT_NAME}.S
 )
 add_custom_command(OUTPUT ${PROJECT_NAME}.sym
     DEPENDS ${PROJECT_NAME}
-    COMMAND ${CMAKE_NM} -C -l -n -S ${PROJECT_NAME} > ${PROJECT_NAME}.sym
+    COMMAND ${CMAKE_NM} ${NM_OPT} ${PROJECT_NAME} > ${PROJECT_NAME}.sym
 )
 
-add_custom_target(srec
-    DEPENDS ${PROJECT_NAME}.srec
-)
 add_custom_target(hex
     DEPENDS ${PROJECT_NAME}.hex
 )
 add_custom_target(bin
     DEPENDS ${PROJECT_NAME}.bin
 )
-add_custom_target(lst
-    DEPENDS ${PROJECT_NAME}.lst
+add_custom_target(assembly
+    DEPENDS ${PROJECT_NAME}.S
 )
 add_custom_target(sym
     DEPENDS ${PROJECT_NAME}.sym
 )
-
