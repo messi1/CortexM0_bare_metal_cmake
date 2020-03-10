84f1923161b814a15c61e994e6803ebc98674afd
bbv_JuergenMesserer <juergen.messerer@bbv.ch>
2020-03-10 22:30:30 +0100

    Beautified the code

--- src/startup/crt1.cpp
+++ src/startup/crt1.cpp
@@ -1,65 +1,60 @@
 //#include <algorithm>
 //#include <iterator>
 
-extern "C"
-{
-  struct ctor_type
-  {
-    typedef void(*function_type)();
-  };
+extern "C" {
+    struct ctor_type {
+      typedef void (*function_type)();
+    };
 
-//  extern ctor_type::function_type _ctors_end[];
-//  extern ctor_type::function_type _ctors_begin[];
-  extern ctor_type::function_type __init_array_start[];
-  extern ctor_type::function_type __init_array_end[];
-  extern ctor_type::function_type __preinit_array_start[];
-  extern ctor_type::function_type __preinit_array_end[];
-  extern ctor_type::function_type __fini_array_start[];
-  extern ctor_type::function_type __fini_array_end[];
+    //  extern ctor_type::function_type _ctors_end[];
+    //  extern ctor_type::function_type _ctors_begin[];
+    extern ctor_type::function_type __init_array_start[];
+    extern ctor_type::function_type __init_array_end[];
+    extern ctor_type::function_type __preinit_array_start[];
+    extern ctor_type::function_type __preinit_array_end[];
+    extern ctor_type::function_type __fini_array_start[];
+    extern ctor_type::function_type __fini_array_end[];
 }
 
-namespace crt
-{
-  void init_ctors();
-  void init_dtors();
-}
+namespace crt {
+    void init_ctors();
+    void init_dtors();
+} // namespace crt
 
-void crt::init_ctors()
-{
-//    auto array = _ctors_begin;
-//    while( array < _ctors_end )
-//    {
-//        (*array)();
-//        array++;
-//    }
+void crt::init_ctors() {
+    //    auto array = _ctors_begin;
+    //    while( array < _ctors_end )
+    //    {
+    //        (*array)();
+    //        array++;
+    //    }
 
     int size = __preinit_array_end - __preinit_array_start;
-    for( int i = 0; i < size; i++ )
-       __preinit_array_start[i]();
+    for (int i = 0; i < size; i++)
+    __preinit_array_start[i]();
 
     size = __init_array_end - __init_array_start;
-    for( int i = 0; i < size; i++ )
-       __init_array_start[i]();
+    for (int i = 0; i < size; i++)
+    __init_array_start[i]();
 
-
-//    array = __init_array_start;
-//    while( array < __init_array_end )
-//    {
-//        (*array)();
-//        array++;
-//    }
+    //    array = __init_array_start;
+    //    while( array < __init_array_end )
+    //    {
+    //        (*array)();
+    //        array++;
+    //    }
 }
 
 void crt::init_dtors()
 {
     int size = __fini_array_end - __fini_array_start;
-    for( int i = 0; i < size; i++ )
-       __fini_array_start[i]();
+    for (int i = 0; i < size; i++)
+    __fini_array_start[i]();
 
-//    auto array = __fini_array_start;
-//    while( array < __fini_array_end )
-//    {
-//        (*array)();
-//        array++;
-//    }
+    //    auto array = __fini_array_start;
+    //    while( array < __fini_array_end )
+    //    {
+    //        (*array)();
+    //        array++;
+    //    }
 }
