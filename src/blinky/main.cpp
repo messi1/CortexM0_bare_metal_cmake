a72d51ac60752b40f16d3f0dcea560d7a975c366
bbv_JuergenMesserer <juergen.messerer@bbv.ch>
2020-03-12 18:36:05 +0100

    Add subdirectory for blinky

---
+++ src/blinky/main.cpp
@@ -0,0 +1,48 @@
+// define used registers
+static volatile unsigned &RCC_AHB1 = *reinterpret_cast<unsigned *>(0x40021014);
+static volatile unsigned &GPIOA_MODER =
+    *reinterpret_cast<unsigned *>(0x48000000);
+static volatile unsigned &GPIOA_BSRR =
+    *reinterpret_cast<unsigned *>(0x48000018);
+
+class A {
+public:
+  virtual void bla() = 0;
+  virtual void gugus() = 0;
+};
+
+class B : public A {
+public:
+  void bla() override {}
+  void gugus() override {}
+};
+
+// application
+int main_app() {
+  B ab;
+  ab.gugus();
+  volatile int x = static_cast<int>(100000l);
+  if (x)
+    ++x;
+  RCC_AHB1 |= 0x00020000;    // enable clock for GPIOA
+  GPIOA_MODER |= 0x40000000; // set output on GPIOA.P15
+  // double x = 0.0;
+  // double y = 0.0;
+  volatile int *bla = new int;
+
+  if (bla) {
+    *bla = 10;
+  }
+
+  for (int i = 0; i < 10; ++i) {
+    *bla = 10 + i;
+  }
+
+  while (true) {
+    // y = sin(x);
+    // x += y;
+    GPIOA_BSRR = 0x00008000; // set output on GPIOA.P15
+    GPIOA_BSRR = 0x80000000; // clear output on GPIOA.P15
+  }
+  return 0;
+}
