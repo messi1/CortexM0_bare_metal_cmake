e823dc6a34751b1dec0edd75d6bd548b470b6d82
bbv_JuergenMesserer <juergen.messerer@bbv.ch>
2020-10-27 22:13:04 +0100

    Add constexpr to each function for register access

--- src/util/reg_access_dynamic.h
+++ src/util/reg_access_dynamic.h
@@ -1,34 +1,57 @@
 #pragma once
 
-namespace util
+namespace util {
+template <typename reg_addr_type, typename reg_value_type>
+
+struct reg_access_dynamic final
 {
-  template<typename reg_addr_type, typename reg_value_type>
+    static constexpr reg_value_type reg_get(const reg_addr_type address)
+    {
+        return *reinterpret_cast<volatile reg_value_type*>(address);
+    }
 
+    static constexpr void reg_set(const reg_addr_type address, const reg_value_type value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) = value;
+    }
+    static constexpr void reg_and(const reg_addr_type address, const reg_value_type value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) &= value;
+    }
+    static constexpr void reg_or(const reg_addr_type address, const reg_value_type value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) |= value;
+    }
+    static constexpr void reg_not(const reg_addr_type address, const reg_value_type value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) &= reg_value_type(~value);
+    }
+    static constexpr void reg_msk(const reg_addr_type address, const reg_value_type value,
+                                  const reg_value_type mask_value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) =
+            reg_value_type(reg_value_type(reg_get(address) & reg_value_type(~mask_value)) |
+                           reg_value_type(value & mask_value));
+    }
 
-  struct reg_access_dynamic final
-  {
-    static reg_value_type
-                reg_get(const reg_addr_type address) { return *reinterpret_cast<volatile reg_value_type*>(address); }
-
-    static void reg_set(const reg_addr_type address, const reg_value_type value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address)  = value; }
-    static void reg_and(const reg_addr_type address, const reg_value_type value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address) &= value; }
-    static void reg_or (const reg_addr_type address, const reg_value_type value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address) |= value; }
-    static void reg_not(const reg_addr_type address, const reg_value_type value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address) &= reg_value_type(~value); }
-    static void reg_msk(const reg_addr_type address, const reg_value_type value, const reg_value_type mask_value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address) = reg_value_type(reg_value_type(reg_get(address) & reg_value_type(~mask_value)) | reg_value_type(value & mask_value)); }
-
-    static void bit_set(const reg_addr_type address, const reg_value_type value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address) |= static_cast<reg_value_type>(1UL << value); }
-    static void bit_clr(const reg_addr_type address, const reg_value_type value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address) &= static_cast<reg_value_type>(~static_cast<reg_value_type>(1UL << value)); }
-    static void bit_not(const reg_addr_type address, const reg_value_type value)
-                        { *reinterpret_cast<volatile reg_value_type*>(address) ^= static_cast<reg_value_type>(1UL << value); }
-    static bool bit_get(const reg_addr_type address, const reg_value_type value)
-                        { return (static_cast<volatile reg_value_type>(reg_get(address) &
-                          static_cast<reg_value_type>(1UL << value)) != static_cast<reg_value_type>(0U)); }
-  };
-}
+    static constexpr void bit_set(const reg_addr_type address, const reg_value_type value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) |= static_cast<reg_value_type>(1UL << value);
+    }
+    static constexpr void bit_clr(const reg_addr_type address, const reg_value_type value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) &=
+            static_cast<reg_value_type>(~static_cast<reg_value_type>(1UL << value));
+    }
+    static constexpr void bit_not(const reg_addr_type address, const reg_value_type value)
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) ^= static_cast<reg_value_type>(1UL << value);
+    }
+    static constexpr bool bit_get(const reg_addr_type address, const reg_value_type value)
+    {
+        return (static_cast<volatile reg_value_type>(reg_get(address) &
+                                                     static_cast<reg_value_type>(1UL << value)) !=
+                static_cast<reg_value_type>(0U));
+    }
+};
+} // namespace util
--- src/util/reg_access_static.h
+++ src/util/reg_access_static.h
@@ -1,29 +1,45 @@
 #pragma once
 
-namespace util
+namespace util {
+template <typename reg_addr_type, typename reg_value_type, const reg_addr_type address,
+          const reg_value_type value = static_cast<reg_value_type>(0)>
+
+struct reg_access_static final
 {
-  template<typename reg_addr_type, typename reg_value_type,
-           const reg_addr_type address, const reg_value_type value = static_cast<reg_value_type>(0)>
+    static constexpr reg_value_type reg_get() { return *reinterpret_cast<volatile reg_value_type*>(address); }
 
-  struct reg_access_static final
-  {
-    static reg_value_type
-                reg_get() { return *reinterpret_cast<volatile reg_value_type*>(address); }
-
-    static void reg_set() { *reinterpret_cast<volatile reg_value_type*>(address)  = value; }
-    static void reg_and() { *reinterpret_cast<volatile reg_value_type*>(address) &= value; }
-    static void reg_or () { *reinterpret_cast<volatile reg_value_type*>(address) |= value; }
-    static void reg_not() { *reinterpret_cast<volatile reg_value_type*>(address) &= reg_value_type(~value); }
-    template<const reg_value_type mask_value>
-    static void reg_msk() { *reinterpret_cast<volatile reg_value_type*>(address) = reg_value_type(reg_value_type(reg_get() &
-                                    reg_value_type(~mask_value)) | reg_value_type(value & mask_value)); }
-
-    static void bit_set() { *reinterpret_cast<volatile reg_value_type*>(address) |= static_cast<reg_value_type>(1ULL << value); }
-    static void bit_clr() { *reinterpret_cast<volatile reg_value_type*>(address) &= static_cast<reg_value_type>(~static_cast<reg_value_type>(1ULL << value)); }
-    static void bit_not() { *reinterpret_cast<volatile reg_value_type*>(address) ^= static_cast<reg_value_type>(1ULL << value); }
-    static bool bit_get() {
-        return (static_cast<volatile reg_value_type>(reg_get() & static_cast<reg_value_type>(1ULL << value)) != static_cast<reg_value_type>(0U));
+    static constexpr void reg_set() { *reinterpret_cast<volatile reg_value_type*>(address) = value; }
+    static constexpr void reg_and() { *reinterpret_cast<volatile reg_value_type*>(address) &= value; }
+    static constexpr void reg_or() { *reinterpret_cast<volatile reg_value_type*>(address) |= value; }
+    static constexpr void reg_not()
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) &= reg_value_type(~value);
     }
-  };
-}
+    template <const reg_value_type mask_value>
+    static constexpr void reg_msk()
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) = reg_value_type(
+            reg_value_type(reg_get() & reg_value_type(~mask_value)) | reg_value_type(value & mask_value));
+    }
 
+    static constexpr void bit_set()
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) |= static_cast<reg_value_type>(1ULL << value);
+    }
+    static constexpr void bit_clr()
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) &=
+            static_cast<reg_value_type>(~static_cast<reg_value_type>(1ULL << value));
+    }
+    static constexpr void bit_not()
+    {
+        *reinterpret_cast<volatile reg_value_type*>(address) ^= static_cast<reg_value_type>(1ULL << value);
+    }
+    static constexpr bool bit_get()
+    {
+        return (
+            static_cast<volatile reg_value_type>(reg_get() & static_cast<reg_value_type>(1ULL << value)) !=
+            static_cast<reg_value_type>(0U));
+    }
+};
+} // namespace util
